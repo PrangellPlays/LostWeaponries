5ed4c784b93f23bfcd8aa12822705da9aca5e8f3
PrangellPlays <ewanprangell@gmail.com>
2022-07-18 20:28:08 +0100

    Initial commit

---
+++ src/main/java/net/prangellplays/lostweaponries/LostWeaponries.java
@@ -0,0 +1,15 @@
+package net.prangellplays.lostweaponries;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LostWeaponries implements ModInitializer {
+	public static final String MOD_ID = "lostweapons";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/net/prangellplays/lostweaponries/LostWeaponriesClient.java
@@ -0,0 +1,10 @@
+package net.prangellplays.lostweaponries;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LostWeaponriesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
