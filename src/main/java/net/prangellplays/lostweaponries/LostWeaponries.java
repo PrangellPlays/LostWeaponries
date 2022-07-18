5c294e8619caabc33912a3cacf85b59c073de6ee
PrangellPlays <ewanprangell@gmail.com>
2022-07-18 20:49:16 +0100

    c

--- src/main/java/net/prangellplays/lostweaponries/LostWeaponries.java
+++ src/main/java/net/prangellplays/lostweaponries/LostWeaponries.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LostWeaponries implements ModInitializer {
-	public static final String MOD_ID = "lostweapons";
+	public static final String MOD_ID = "lostweaponries";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
