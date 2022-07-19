ede83e9bac193b1d61389abcceb3a6005764bab6
PrangellPlays <ewanprangell@gmail.com>
2022-07-19 16:23:38 +0100

    c

--- src/main/java/net/prangellplays/lostweaponries/item/ModItems.java
+++ src/main/java/net/prangellplays/lostweaponries/item/ModItems.java
@@ -3,12 +3,16 @@ package net.prangellplays.lostweaponries.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.prangellplays.lostweaponries.LostWeaponries;
 
 public class ModItems {
     public static final Item DUCK_BLADE = registerItem("duck_blade",
+            new SwordItem(ModToolMaterials.Duck, 1, 2f, new FabricItemSettings().group(ModItemGroup.LostWeaponries.setNoScrollbar())));
+
+    public static final Item DUCK_INGOT = registerItem("duck_ingot",
             new Item(new FabricItemSettings().group(ModItemGroup.LostWeaponries.setNoScrollbar())));
 
 
