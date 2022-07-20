8acf79489e76db5f1dbbc1e754c2187e7075ec3a
PrangellPlays <ewanprangell@gmail.com>
2022-07-20 17:09:21 +0100

    c

--- src/main/java/net/prangellplays/lostweaponries/LostWeaponries.java
+++ src/main/java/net/prangellplays/lostweaponries/LostWeaponries.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.prangellplays.lostweaponries.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.bernie.geckolib3.GeckoLib;
 
 public class LostWeaponries implements ModInitializer {
 	public static final String MOD_ID = "lostweaponries";
@@ -13,6 +14,7 @@ public class LostWeaponries implements ModInitializer {
 	public void onInitialize() {
 
 		ModItems.registerModItems();
+		GeckoLib.initialize();
 		
 	}
 }
